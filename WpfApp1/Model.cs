6f09b0248d9e4ec0b354b0867cb9c53f27953b74
Admin <Admin@DESKTOP-HC1CC25>
2024-12-25 10:06:45 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Model.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Model
+    {
+        public double FirstNumber { get; set; }
+
+        public double SecondNumber { get; set; }
+
+        public string Operator { get; set; }
+
+        public double Calculate()
+        {
+            return Operator switch
+            {
+                "+" => FirstNumber + SecondNumber,
+                "-" => FirstNumber - SecondNumber,
+                "*" => FirstNumber * SecondNumber,
+
+                "/" => SecondNumber != 0 ? FirstNumber / SecondNumber : 0,
+                _=> 0,
+            };
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ WpfApp1/ViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Model _model;
+        private string _display;
+        private string _operator;
+        private bool _isoperatorClick;
+
+        public ICommand NumberCommand { get; set; }
+
+        public ICommand OperatorCommand { get; set; }
+
+        public ICommand ClearCommand { get; set; }
+
+        public ICommand CalculateCommand { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ViewModel()
+        {
+            _model = new Model();
+            _display = "0";
+            _isoperatorClick = false;
+
+            NumberCommand = new RelayCommand(OnNumberClick);
+            OperatorCommand = new RelayCommand(OnOperatorClick);
+            ClearCommand = new RelayCommand(OnClearClicked);
+            CalculateCommand = new RelayCommand(OnCalculateClick);
+
+        }
+
+        public string DisplayText
+        {
+            get { return _display; }
+            set { _display = value; OnPropertyChanged(nameof(DisplayText)); }
+        }
+
+
+
+        private void OnCalculateClick(object obj)
+        {
+            DisplayText = _model.Calculate().ToString();
+            _model.FirstNumber = double.Parse(DisplayText);
+        }
+
+        private void OnClearClicked(object obj)
+        {
+            DisplayText = "0";
+            _model.FirstNumber = 0;
+            _model.SecondNumber = 0;
+            _operator = null;
+        }
+
+        private void OnOperatorClick(object obj)
+        {
+            _operator = obj.ToString();
+            _model.Operator = _operator;
+            _isoperatorClick = true;
+        }
+
+        private void OnNumberClick(object obj)
+        {
+            if (_isoperatorClick || DisplayText == "0")
+            {
+                DisplayText = string.Empty;
+            }
+            DisplayText += obj?.ToString();
+            _isoperatorClick = false;
+            if (_operator == null)
+            {
+                _model.FirstNumber = double.Parse(DisplayText);
+            }
+            else
+            {
+                _model.SecondNumber = double.Parse(DisplayText);
+            }
+        }
+    }
+}  
